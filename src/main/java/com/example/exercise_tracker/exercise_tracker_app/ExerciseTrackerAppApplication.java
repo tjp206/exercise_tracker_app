583714c08fe5f5b9d7f31675255cef99c70d3258
TJ <tjpataria@TJs-MacBook-Pro.local>
2021-02-23 11:36:38 +0000

    repository initialised & set up complete for Spring Boot dependencies

---
+++ src/main/java/com/example/exercise_tracker/exercise_tracker_app/ExerciseTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercise_tracker.exercise_tracker_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exercise_tracker/exercise_tracker_app/ExerciseTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise_tracker.exercise_tracker_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
