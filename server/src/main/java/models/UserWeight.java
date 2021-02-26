7a25ddcadcf7c0e7e4e7a5129588035f6545d374
TJ <tjpataria@TJs-MacBook-Pro.local>
2021-02-26 15:25:48 +0000

    created UserWeight class

--- server/src/main/java/models/UserWeight.java
+++ server/src/main/java/models/UserWeight.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class UserWeight {
 
+    Long id;
     private double weight;
     private LocalDate date;
 
@@ -13,6 +14,14 @@ public class UserWeight {
     }
 
     public UserWeight() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public double getWeight() {
         return weight;
