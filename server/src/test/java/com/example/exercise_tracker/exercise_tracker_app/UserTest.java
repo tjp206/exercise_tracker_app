c785b1d8bd198226a5805c4e9e6f7984c8e2315d
TJ <tjpataria@gmail.com>
2021-03-17 14:10:41 +0000

    added method to return age based on dob - tests passing.
    Co-authored-by: Linsey Currie <linseycurrie@gmail.com>
    Co-authored-by: David Strain <Strd79@hotmail.co.uk>

---
+++ server/src/test/java/com/example/exercise_tracker/exercise_tracker_app/UserTest.java
@@ -0,0 +1,27 @@
+package com.example.exercise_tracker.exercise_tracker_app;
+
+import com.example.exercise_tracker.exercise_tracker_app.models.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class UserTest {
+
+    User user;
+
+    @Before
+    public void before() {
+        user = new User("TJ", "Pataria", LocalDate.of(1986, Month.OCTOBER, 1), "male", "tj@gmail.com", 183);
+    }
+
+    @Test
+    public void canGetAge() {
+        assertEquals(34, user.getAge(), 0.01);
+    }
+}
