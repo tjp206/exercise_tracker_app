a568a01eec9dacd465339aa9730770b950622fbf
TJ <tjpataria@gmail.com>
2021-03-24 10:48:38 +0000

    added get/post/patch/delete methods to request.js

---
+++ client/src/helpers/request.js
@@ -0,0 +1,39 @@
+class Request {
+
+    // constructor() {
+    //   this.base = "";
+    // }
+  
+      get(url) {
+        return fetch(url, {
+          method: "GET",
+          headers: {'Content-Type': 'application/json'}
+          })
+        .then((res) => res.json());
+      }
+  
+      delete(url) {
+        return fetch(url, {
+          method: "DELETE",
+          headers: {'Content-Type': 'application/json'}
+        })
+      }
+  
+      post(url, payload) {
+        return fetch(url, {
+          method: "POST",
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(payload)
+        })
+      }
+  
+      patch(url, payload) {
+        return fetch(url, {
+          method: "PATCH",
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(payload)
+        })
+      }
+  }
+  
+  export default Request;
