024f910fd294168b55c046a4eb48b449632651b0
David Strain <strd79@hotmail.co.uk>
2021-03-26 13:41:05 +0000

    Updated fields on registration form and added some css
    Co-authored-by: TJ Pataria <tjpataria@gmail.com>
    Co-authored-by: Linsey Currie <linseycurrie@gmail.com>

--- client/src/components/RegistrationComponent.js
+++ client/src/components/RegistrationComponent.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './RegistrationComponent.css';
 
 
 function RegistrationComponent(props){
@@ -7,10 +8,11 @@ function RegistrationComponent(props){
         firstName: "",
         lastName: "",
         dob: "",
-        gender: "",
+        sex: "",
         height: "",
         email: "",
-        password: ""
+        password: "",
+        confirmPassword: ""
     })
 
     const handleChange = (e) => {
@@ -22,6 +24,7 @@ function RegistrationComponent(props){
     }
 
     const handleSubmitClick = (e) => {
+        console.log(e)
         e.preventDefault();
         if(state.password === state.confirmPassword) {
             // sendDetailsToServer()    
@@ -36,61 +39,96 @@ function RegistrationComponent(props){
             <label htmlFor="firstName">First Name: </label>
                 <input 
                     id="firstName"
+                    name="firstName"
                     type="text"
-                    placeholder="Enter First Name"
+                    className="form-control"
+                    placeholder="Bruce"
+                    required
                     onChange={handleChange}
                 />
+                <br /><br />
+
                 <label htmlFor="lastName">Last Name: </label>
                 <input 
                     id="lastName"
+                    name="lastName"
                     type="text"
-                    placeholder="Enter Last Name"
+                    className="form-control"
+                    placeholder="Wayne"
+                    required
                     onChange={handleChange}
                 />
+                <br /><br />
+
                 <label htmlFor="dob">Date of Birth: </label>
                 <input 
                     id="dob"
+                    name="dob"
                     type="date"
-                    placeholder="Enter date of birth"
+                    className="form-control"
+                    placeholder="Select your date of birth"
+                    required
                     onChange={handleChange}
                 />
-                <br></br>
-                <label>Gender:</label><br></br>
-                <input type="radio" id="male" name="gender" value="male" onChange={handleChange}/>
-                <label for="male">Male</label>
-                <input type="radio" id="female" name="gender" value="female" onChange={handleChange}/>
-                <label for="female">Female</label>
-                <input type="radio" id="other" name="gender" value="other" onChange={handleChange}/>
-                <label for="other">Other</label>
-                <br></br>
+                <br /><br />
+
+                <label htmlFor="sex">Sex: </label>
+                <select id="sex" name="sex" className="form-control" onChange={handleChange}>
+                    <option value="" disabled selected>- Please select your sex -</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                </select>
+                <br /><br />
 
                 <label htmlFor="height">Height: </label>
-                <input className="height"
+                <input 
+                    id="height"
+                    name="height"
                     type="number"
-                    placeholder="Enter height"
+                    className="form-control"
+                    placeholder="185"
+                    required
                     onChange={handleChange}
                 />
-
+                <label> cm</label>
+                <br /><br />
                 
-                <label htmlFor="inputEmail">Email Address: </label>
-                <input className="registerEmail"
+                <label htmlFor="email">Email Address: </label>
+                <input
+                    id="email"
+                    name="email"
                     type="email"
-                    placeholder="Enter email"
+                    className="form-control"
+                    placeholder="batman@wayne-enterprises.com"
+                    required
                     onChange={handleChange}
                 />
-                <label htmlFor="registerPassword">Password: </label>
-                <input className="registerPassword"
-                    type="password"
-                    placeholder="Password"
-                    onChange={handleChange}
-                ></input>
+                <br /><br />
 
-                <label htmlFor="registerPassword">Confirm Password: </label>
-                <input className="registerPassword"
+                <label htmlFor="password">Password: </label>
+                <input
+                    id="password"
+                    name="password"
                     type="password"
-                    placeholder="Confirm password"
+                    className="form-control"
+                    placeholder="Create password"
+                    required
                     onChange={handleChange}
-                ></input>
+                />
+                <br /><br />
+
+                <label htmlFor="confirmPassword">Confirm Password: </label>
+                <input
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    type="password"
+                    className="form-control"
+                    placeholder="Confirm password"
+                    required
+                    onChange={handleChange}
+                />
+                <br /><br />
 
                 <button type="submit" onClick={handleSubmitClick}>Register</button>
             </form>
