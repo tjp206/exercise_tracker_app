ecf6e421d6ea4ce950017661f49724793d363c5d
David Strain <strd79@hotmail.co.uk>
2021-04-09 11:04:56 +0100

    Added <p> tags to DashboardComponent
    Co-authored-by: TJ Pataria <tjpataria@gmail.com>

--- client/src/components/DashboardComponent.js
+++ client/src/components/DashboardComponent.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function Dashboard() {
   return(
+    <>
     <h2>Dashboard</h2>
+    <p>This is where users will land once they have logged in.</p>
+    <p>They can then navigate to different parts of the app.</p>
+    </>
   );
 }
